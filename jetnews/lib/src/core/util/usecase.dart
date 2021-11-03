9e878bd3a1d319becf33040bc65320e01b222367
gwanhyeong <wudy343@gmail.com>
2021-11-03 21:38:59 +0900

    make usecase class to callable

--- jetnews/lib/src/core/util/usecase.dart
+++ jetnews/lib/src/core/util/usecase.dart
@@ -1,10 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
-import '../error/failures.dart';
-
 abstract class UseCase<DataType, Params> {
-  Future<Either<Failure, DataType>> execute(Params params);
+  Future<Either<Exception, DataType>> call(Params params);
 }
 
 class NoParams extends Equatable {
